231c8b9b5a5379c70c2e82931eb8868a21e2dacc
piranav <76738194+piranav@users.noreply.github.com>
2023-07-05 10:40:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
-# Import os to set API key
+
 import os
-# Import OpenAI as main LLM service
 from langchain.llms import OpenAI
 from langchain.embeddings import OpenAIEmbeddings
-# Bring in streamlit for UI/app interface
 import streamlit as st
-
-# Import PDF document loader
 from langchain.document_loaders import PyPDFLoader
-# Import chroma as the vector store 
 from langchain.vectorstores import Chroma
-
-# Import vector store stuff
 from langchain.agents.agent_toolkits import (
     create_vectorstore_agent,
     VectorStoreToolkit,
     VectorStoreInfo
 )
 
-# Set APIkey for OpenAI Service
-os.environ['OPENAI_API_KEY'] = ''
+# Set API key for OpenAI Service
+os.environ['OPENAI_API_KEY'] = 'YOUR_OPEN_AI_KEY'
 
-# Create instance of OpenAI LLM
+# Create an instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
 embeddings = OpenAIEmbeddings()
 # Create and load PDF Loader
@@ -31,7 +24,7 @@ pages = loader.load_and_split()
 # Load documents into vector database aka ChromaDB
 store = Chroma.from_documents(pages, embeddings, collection_name='designdesigner')
 
-# Create vectorstore info object - metadata repo?
+# Create vectorstore info object
 vectorstore_info = VectorStoreInfo(
     name="designdesigner",
     description="a digital designer's guide",
@@ -50,14 +43,11 @@ st.title('👾 GPT Digital Design Engineer')
 # Create a text input box for the user
 prompt = st.text_input('Input your prompt here')
 
-# If the user hits enter
+#Prompt logic
 if prompt:
-    # Then pass the prompt to the LLM
     response = agent_executor.run(prompt)
-    # ...and write it out to the screen
     st.write(response)
-
-    # With a streamlit expander  
+    
     with st.expander('Document Similarity Search'):
         # Find the relevant pages
         search = store.similarity_search_with_score(prompt) 
